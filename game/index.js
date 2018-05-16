6c45d4bd01809b147231abe5e21cba0e71f98307
dk268 <lorddanp@aol.com>
2018-05-16 12:25:52 -0400

    basic functionality functioning

--- game/index.js
+++ game/index.js
@@ -1,6 +1,34 @@
-import {Map} from 'immutable'
+import { Map } from "immutable";
 
-export default function reducer(state, action) {
-  // TODO
-  return state
+let board = Map();
+
+const X = "X";
+const O = "O";
+const MOVE = "MOVE";
+const START = "START";
+
+const move = position => ({
+  type: "MOVE",
+  position
+});
+const POSITION = "POSITION";
+
+function gameReducer(state = { board, counter: 1, turn: X }, action) {
+  switch (action.type) {
+    case MOVE: {
+      const player = state.counter % 2 ? X : O;
+      return {
+        board: state.board.setIn(action.position, player),
+        counter: state.counter + 1,
+        turn: player
+      };
+    }
+    default:
+      return state;
+  }
 }
+
+module.exports = {
+  gameReducer,
+  move
+};
